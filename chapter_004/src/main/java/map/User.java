aff04fe567cc615fae6feb8b4f1fed7a951d044c
IBryt <ihorbryt@gmail.com>
2018-08-29 19:14:03 +0300

    5. Перекрывать и equals и hashCode [#1002][#68424]

--- chapter_004/src/main/java/map/User.java
+++ chapter_004/src/main/java/map/User.java
@@ -62,4 +62,28 @@ public class User {
         result = 31 * result + (birthday != null ? birthday.hashCode() : 0);
         return result;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
 }
--- chapter_004/src/test/java/map/UserTest.java
+++ chapter_004/src/test/java/map/UserTest.java
@@ -4,6 +4,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class UserTest {
 
@@ -19,5 +25,13 @@ public class UserTest {
     @Test
     public void whenCallMapUsersReturnMessage() {
         new User().mapUsers(u1, u2);
+    }
+
+    @Test
+    public void whenChangeFieldKeyAndFindByTheKeyReturnNull() {
+        Map map = new HashMap<User, String>();
+        map.put(u1, "test");
+        u1.setChildren(2);
+        assertThat(map.get(u1), is(nullValue()));
     }
 }
