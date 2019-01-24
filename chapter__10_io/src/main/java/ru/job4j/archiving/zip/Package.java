4705874a5f2c45b9e7ac96093797a0300f2faf78
IBryt <ihorbryt@gmail.com>
2019-01-24 23:41:06 +0200

    4. Архивировать проект. [861][#108340]

--- chapter__10_io/src/main/java/ru/job4j/archiving/zip/Package.java
+++ chapter__10_io/src/main/java/ru/job4j/archiving/zip/Package.java
@@ -1,8 +1,6 @@
 package ru.job4j.archiving.zip;
 
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.SeekableByteChannel;
 import java.nio.file.*;
 import java.util.*;
 import java.util.zip.ZipEntry;
@@ -12,10 +10,6 @@ import java.util.zip.ZipOutputStream;
  * This class intended for create archives files.
  */
 public class Package {
-    /**
-     *  size buffer in bytes.
-     */
-    private static final int SIZE_BUFFER = 4096;
 
     /**
      * Create archive file with <code>files</code> of preserving relative paths relatively <code>parentPath</code>.
@@ -36,28 +30,15 @@ public class Package {
                 String pathZIP = Paths.get(parentPath).relativize(pathFile).toString();
                 addFile(zos, pathZIP, pathFile);
             }
-            zos.closeEntry();
+            zos.finish();
         }
         return pathTarget.getFileName().toAbsolutePath().toString();
     }
 
     private void addFile(ZipOutputStream zos, String pathZIP, Path pathFile) throws IOException {
-        int count;
-        try (SeekableByteChannel channel = Files.newByteChannel(pathFile)) {
-            ZipEntry e = new ZipEntry(pathZIP);
-            zos.putNextEntry(e);
-            ByteBuffer buffer = ByteBuffer.allocate(SIZE_BUFFER);
-            do {
-                count = channel.read(buffer);
-                if (count != -1) {
-                    buffer.rewind();
-                    byte[] bytes = buffer.array();
-                    if (count != bytes.length) {
-                        bytes = Arrays.copyOf(bytes, count);
-                    }
-                    zos.write(bytes);
-                }
-            } while (count != -1);
-        }
+        ZipEntry e = new ZipEntry(pathZIP);
+        zos.putNextEntry(e);
+        Files.copy(pathFile, zos);
+        zos.closeEntry();
     }
 }
