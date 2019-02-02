11241b7d4c45865bf3af486f893fd0e4e97009e8
IBryt <ihorbryt@gmail.com>
2019-02-02 15:39:09 +0200

    1. Создать TODO list [#3786]

---
+++ chapter_011_todolist/src/test/java/ru/job4j/servlets/ControllerItemsTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.servlets;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+import static org.mockito.Mockito.when;
+
+public class ControllerItemsTest {
+    private ControllerItems controller = new ControllerItems();
+
+    @Before
+    public void before() throws ServletException {
+        controller.init();
+    }
+
+    @After
+    public void after() {
+        controller.destroy();
+    }
+
+//    @Test
+//    public void test() throws ServletException, IOException {
+//        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
+//        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
+//        when(request.getParameter("done")).thenReturn("false");
+//        controller.doGet(request, response);
+//    }
+}
